778dc55a415e8fbe4296557c3fe7b7a7ca1b260d
vkrm03 <vikramrokith03@gmail.com>
2025-08-02 23:21:39 +0530

    NotFound Page redirect fixed

--- frontend/Pay-Roll-Management/src/Pages/NotFound.jsx
+++ frontend/Pay-Roll-Management/src/Pages/NotFound.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../public/styles/not_found.css';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const role = localStorage.getItem('role');
+
+    if (token && role) {
+
+      const timeout = setTimeout(() => {
+        if (role === 'admin') {
+          navigate('/dashboard');
+        } else {
+          navigate('/user_dash');
+        }
+      }, 2000);
+
+      return () => clearTimeout(timeout);
+    } else {
+      const timeout = setTimeout(() => {
+          navigate('/');
+      }, 2000);
+    }
+  }, [navigate]);
+
   return (
     <div className="not-found">
       <img
-        src="../public\imgs\404.png"
-        alt="Payroll Illustration"
+        src="/imgs/404.png"
+        alt="Page Not Found"
         className="not-found-image"
       />
       <h1>404</h1>
       <p>Oops! The page you’re looking for doesn’t exist.</p>
-      <Link to="/" className="home-button">Go to Home</Link>
+      <p>Redirecting you to Home...</p>
     </div>
   );
 };
